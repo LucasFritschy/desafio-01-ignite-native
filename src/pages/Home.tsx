a8cbca24659b1d2f851b8dee860501bd69f93083
Lucas Fritschy <lucasfritschy@gmail.com>
2021-07-23 18:00:27 -0300

    teste concluido

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,17 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks([...tasks, { id: new Date().getTime(), title: newTaskTitle, done: false }])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(
+      tasks.map(task => task.id === id ? { ...task, done: !task.done } : task)
+    )
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
@@ -26,10 +28,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
